8a0339af4cb473a62676e2028d5bf04caf6b101f
Win10Bogdan <vba@colostate.ede>
2020-09-23 15:25:15 -0600

    Hw2 almost Done

--- HW2/Checker.c
+++ HW2/Checker.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h>
+#include <sys/types.h>
+
 #include "Checker.h"
 
-void doo(){
+int main(int argc, char **argv)
+{
+    int id;
+    id =  getpid();
+    printf("Checker process [%u]: Starting.", id); 
+    int x = atoi(argv[1]);
+    int y = atoi(argv[2]);
+    
+    if (y % x == 0){
+        printf("Checker process [%u]: %d *IS* divisible by %d.", id, y, x);
+        printf("Checker process [%u]: Returning %d.", id, 1);
+        return 1;
+    }else{
+        printf("Checker process [%u]: %d *IS NOT* divisible by %d.", id, y, x);
+        printf("Checker process [%u]: Returning %d.", id, 0);
+        return 0; 
+    }
+    
 
-    printf("Hi");
 }
--- HW2/Coordinator.c
+++ HW2/Coordinator.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <fcntl.h>
+#include <unistd.h> 
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <sys/resource.h>
+#include <sys/mman.h>
 
-int main()
+
+
+int main(int argc, char **argv)
 {
+    if (argc != 6){
+        printf("Enter only 5 arguments!\n");
+        return 1;
+
+    }
+    int id;
+    for(int i = 0; i < 4; i++){
+
+        id=fork();
+
+        if(id == 0) {
+            //Child
+            execlp("checker", "checker", argv[1], argv[i+2], NULL);
+            exit(0);
+
+        }else{
+            printf("Coordinator: forked process with ID %u.\n", id);
+            printf("Coordinator: wainting for process [%u].\n", id);
+            int updt;
+
+            waitpid (id, &updt, 0);
+            if(WIFEXITED(updt)){
+                int status = WEXITSTATUS(updt);
+                printf("Coordinator: child process %u retuned: %d.\n", id, status);
+                continue;
+            }
+
+        }
+    }
+    printf("Coordinator: exiting.\n");
+
     return 0;
 }
