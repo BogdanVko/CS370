afc7ab8dc4d52ccea92f06996d3a019b304203a1
Win10Bogdan <vba@colostate.ede>
2020-09-30 16:22:22 -0600

    hw3 done

--- HW2/Checker.c
+++ HW2/Checker.c
@@ -1,48 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h> 
+
+#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdio.h>
+#include <sys/shm.h>
+#include <stdlib.h>
 
-#include "Checker.h"
-
-int main(int argc, char **argv)
-{  
-
-    
-    int n;
-    read(atoi(argv[0]), &n, sizeof(int));
-    close(argv[0]);
+int main(int argc, char *argv[]){
+	
 
 
-    int* ptr =(int *) shmat(n, NULL, 0);
-    
 
-    if (argc != 3){
-        printf("Enter 3 arguments!\n");
-        return 0;
 
-    }
-    
-    int id;
-    id =  getpid();
-    printf("Checker process [%u]: read 4 bytes containing shm ID %d\n", id, atoi(argv[1])); 
-    int x = atoi(argv[2]);
-    int y = atoi(argv[3]);
-    
-    if (y % x == 0){
-        printf("Checker process [%u]: %d *IS* divisible by %d.\n", id, y, x);
-        memset(ptr, 1, sizeof(int));
+
+    int prog_ID = atoi(argv[0]);
+    int devisor= atoi(argv[1]);
+	int devider= atoi(argv[2]);
+	
+
+
+
+	printf("Checker process [%d]: starting.\n", getpid());
+    int shmID;
+	read(prog_ID, &shmID, sizeof(int));
+	close(prog_ID);
+
+	printf("Checker process [%d]: read %d bytes containing shm ID %d\n", getpid(), (int) sizeof(shmID), shmID);
+
+	int *shared_ptr = shmat(shmID, NULL, 0);
+        ///Check for errors
+	
+	if ( devider % devisor == 0 ){
+		printf("Checker process [%d]: %d *IS* divisible by %d.\n", getpid(), devider, devisor);
+		memset(shared_ptr, 1, sizeof(int));
 		printf("Checker process [%d]: wrote result (1) to shared memory.\n", getpid());
-    }else{
-        printf("Checker process [%u]: %d *IS NOT* divisible by %d.\n", id, y, x);
-        memset(ptr, 0, sizeof(int));
+	}
+	else {
+		printf("Checker process [%d]: %d *IS NOT* divisible by %d.\n", getpid(), devider, devisor);
+		memset(shared_ptr, 0, sizeof(int));
 		printf("Checker process [%d]: wrote result (0) to shared memory.\n", getpid());
-    }
+	}
 
-    int shret = shmdt(ptr);
+	int sharereturn = shmdt(shared_ptr);
 
-	if (shret == -1){
-		perror("BAD RETURN");
-        return 1;
+	if (sharereturn == -1)
+		exit(1);
 
+	return 0;
 }
--- HW2/Coordinator.c
+++ HW2/Coordinator.c
@@ -1,80 +1,119 @@
-#include <stdio.h>               
-#include <stdlib.h>       
-#include <fcntl.h>           
-#include <unistd.h>        
-#include <sys/wait.h>    
-#include <sys/ipc.h>        
-#include <sys/shm.h>              
-#include <sys/types.h>     
-#include <sys/sem.h>            
-#include <sys/resource.h>           
-#include <sys/mman.h>           
-        
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <string.h>
+#include "Checker.h"
 
 
-int main(int argc, char **argv)
+
+
+int main(int argc, char *argv[])
 {
-    if (argc != 6){
-        printf("Enter only 5 arguments!\n");
-        return 0;
 
-    }
     
-    for(int i = 0; i < 4; i++){
-    int id;
-    int p[2];
-        
-    pipe(p);
-        
-        id=fork();
-        close(p[0]); 
-        int shmID = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666); //get mem
-        printf("Coordinator: Created shared memory with ID: %d\n", shmID);
+    
 
-        int *shmPtr = (int *) shmat(shmID, NULL, 0);
+    int c[4];
 
-        
-        
+    
+    
+    int frID[2]; ///pipe states
+    int shID[4]; //IDs
 
-         
-        if (id < 0){
-            fprintf(stderr, "Process Failed!");
+
+
+    int a_counter = 2;
+    while (a_counter < argc) // 4-2 =2
+    {
+
+        if (pipe(frID) == -1)
+        {
             exit(1);
         }
-        if(id == 0) {
-            //Child
-            write(p[1], &shmID, sizeof(int));
-            close(p[1]);
-            char str[4*sizeof(char)];
-            sprintf(str, "%d", 42);
-
-            execlp("./checker", "Checker", str, argv[1], argv[i+2], NULL);
-            
-        }else{ //Parent
-
-            printf("Coordinator: forked process with ID %u.\n", id);
-            printf("Coordinator: wainting for process [%u].\n", id);
-            int updt;
 
 
-// 
-            printf("Coordinator: forked process with ID %d.\n", id);
-			close(p[0]);
-			write(p[1], &shmID, sizeof(int));
-			close(p[1]);
-			printf("Coordinator: wrote shm ID %d to pipe (%d bytes)\n", shmID, (int) sizeof(shmID));
-// 
-            waitpid(id, &updt, 0);
-            if(WIFEXITED(updt)){
-                int status = WEXITSTATUS(updt);
-                printf("Coordinator: child process %u retuned: %d.\n", id, status);
-                continue;
-            }
+        char str[30 * sizeof(char)];
+        sprintf(str, "%d", frID[0]);
 
+
+        shID[a_counter - 2] = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+
+
+
+
+
+        
+
+        if ((c[a_counter - 2] = fork()) == 0)
+        {
+            close(frID[1]);
+
+           // printf("DERBUG: passing args: %d, %d" , atoi(argv[1], atoi()))
+            execlp("./checker",  str, argv[1], argv[a_counter], NULL);
         }
-        //printf("Coordinator: exiting.\n");
+        else
+        {
+            printf("Coordinator: forked process with ID %d.\n", c[a_counter - 2]);
+            close(frID[0]);
+
+            write(frID[1], &shID[a_counter - 2], sizeof(int)); ///write and don't forget to close!!
+            close(frID[1]);
+            printf("Coordinator: wrote shm ID %d to pipe (%d bytes)\n", shID[a_counter - 2], (int)sizeof(shID[a_counter - 2]));
+        }
+        a_counter++;
     }
-    
+    int actual_count = 0;
+    while (actual_count < 4)
+    {
+
+
+
+
+
+
+        printf("Coordinator: waiting on child process ID %d...\n", c[actual_count]);
+        waitpid(c[actual_count], NULL, 0);
+
+        int *shared_ptr = shmat(shID[actual_count], NULL, 0);
+
+        if (shared_ptr == (void *)-1)
+           exit(1);
+        
+
+        int result_val;
+
+        memcpy(&result_val, shared_ptr, 1); // checking
+
+        int shmdt_return = shmdt(shared_ptr);
+
+        if (shmdt_return == -1)
+            exit(1);
+
+        if (shmctl(shID[actual_count], IPC_RMID, NULL) == -1)
+            exit(1);
+
+
+        int devisor = atoi(argv[1]);
+        int arg_dev = atoi(argv[actual_count + 2]);
+        if (result_val == 0)
+        {
+            printf("Coordinator: result %d read from shared memory: %d is not divisible by %d.\n", result_val, arg_dev, devisor);
+        }
+
+        
+        else
+        {
+            printf("Coordinator: result %d read from shared memory: %d is divisible by %d.\n", result_val, arg_dev, devisor);
+        }
+        actual_count++;
+    }
+
+
+
     printf("Coordinator: exiting.\n");
+
     return 0;
 }
